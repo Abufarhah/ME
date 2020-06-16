cba28d55ad10e7512ab974583bcb0d6493fb2538
Abufarhah <laythabufarhah@gmail.com>
2020-06-16 11:01:19 +0300

    Logs was added

--- src/main/java/com/example/me/service/BundleService.java
+++ src/main/java/com/example/me/service/BundleService.java
@@ -2,6 +2,8 @@ package com.example.me.service;
 
 import com.example.me.Bundle;
 import com.example.me.dao.BundleDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,7 @@ import javax.annotation.PostConstruct;
 
 @Service
 public class BundleService {
+	private static final Logger log = LoggerFactory.getLogger(BundleService.class);
 
 	@Autowired
 	BundleDao bundleDao;
@@ -18,14 +21,20 @@ public class BundleService {
 	}
 
 	public Bundle getBundle(int id) {
+		log.info("service to get bundle: " + id);
+
 		return bundleDao.getBundle(id);
 	}
 
 	public String addBundle(int id, String name, double price) {
+		log.info("service to add bundle: " + id);
+
 		return bundleDao.addBundle(id,name,price);
 	}
 
 	public String deleteBundle(int id) {
+		log.info("service to delete bundle: " + id);
+
 		return bundleDao.deleteBundle(id);
 	}
 }
